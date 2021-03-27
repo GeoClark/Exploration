900bc04bfb9a92f7ebf7389d1960ea20e5f02b67
GeoClark <ckitclark@gmail.com>
2021-03-27 13:57:54 -0600

    Created project and data set

---
+++ Edmunds.R
@@ -0,0 +1,19 @@
+# Playing with Car data from Edmunds
+
+#load libraries
+library(dplyr)
+
+2019 CX9
+Dimensions: 199″ L x 78″ W x 68″ H
+MPG: Up to 22 city / 28 highway
+Towing capacity: 3,500 lbs
+Cargo volume: 14.4 ft³, 71.2 ft³ with seat area
+Horsepower: 227 hp
+
+2014 Honda CRV
+MPG: Up to 23 city / 31 highway
+Curb weight: 3,305 to 3,529 lbs
+Towing capacity: 1,500 lbs
+Dimensions: 178″ L x 72″ W x 65″ H
+178/12
+Engine: 2.4 L 4-cylinder
